e98eae3a3547735698798b7fb322776ee66f24af
gaoissac2019 <48165303+gaoissac2019@users.noreply.github.com>
2023-05-26 12:07:46 +0200

    add page content and format

--- main.py
+++ main.py
@@ -9,21 +9,22 @@ url = f"https://api.nasa.gov/planetary/apod?api_key={api_key}"
 response = requests.get(url)
 content = response.json()
 # date
-print(content['date'])
-print(content)
-print(content['explanation'])
-
+date = content['date']
+explanation = content['explanation']
 # get the picture from the dataframe
 picture_url = content['hdurl']
 pic_response = requests.get(picture_url)
 picture_content = pic_response.content
-print(picture_content)
 with open("image.jpg", "wb") as file:
     file.write(picture_content)
+# add streamlit page
+st.set_page_config(page_title="Today's Universe",
+                   page_icon="🧊")
+# add page content
+st.markdown("<h1 style='text-align: center; color: grey;'>Today's Universe</h1>", unsafe_allow_html=True)
+st.markdown(f"<h2 style='text-align: center; color: black;'>{date}</h2>", unsafe_allow_html=True)
+st.image("./image.jpg")
+st.info(explanation)
 
-#st.set_page_config(page_title="Today's Universe",
-#                   page_icon="🧊", layout="centred")
-
-#st.header("Today's Universe")
 
 
